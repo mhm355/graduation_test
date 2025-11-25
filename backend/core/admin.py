3b2d31399aaf653d4244a32fc5062eb7677af975
Mahmoud Hussein <mahmoud.355.mhm@gmail.com>
2025-11-25 14:51:47 +0200

    update

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -10,11 +10,6 @@ class CourseAdmin(admin.ModelAdmin):
     list_display = ('code', 'name', 'department', 'credit_hours')
     list_filter = ('department',) # Adds a filter sidebar
 
-@admin.register(Grade)
-class GradeAdmin(admin.ModelAdmin):
-    list_display = ('student', 'course', 'score', 'semester', 'letter_grade')
-    list_filter = ('course', 'semester')
-    search_fields = ('student__username', 'course__code')
 
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
